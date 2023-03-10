91d8d42579a00de929d7181d27e44dc2f0b61957
Roman_Kotov <roma_kotovo@tr.com>
2023-03-10 23:05:50 +0100

    merge to sorted arrays

---
+++ src/main/java/leetcode/algorithms/MergeToSortedArrays.java
@@ -0,0 +1,45 @@
+package leetcode.algorithms;
+
+import java.util.Arrays;
+
+public class MergeToSortedArrays {
+
+    public static void main(String[] args) {
+        int[] first = {1, 3, 7};
+        int[] second = {2, 4, 6, 9};
+
+        int[] merge = merge(first, second);
+        System.out.println(Arrays.toString(merge));
+    }
+
+    public static int[] merge(int[] first, int[] second) {
+        int[] result = new int[first.length + second.length];
+
+        int indexOfFirst = 0;
+        int indexOfSecond = 0;
+        int indexOfResult = 0;
+
+        while (indexOfFirst < first.length && indexOfSecond < second.length) {
+            if (first[indexOfFirst] < second[indexOfSecond]) {
+                result[indexOfResult] = first[indexOfFirst];
+                indexOfFirst++;
+            } else {
+                result[indexOfResult] = second[indexOfSecond];
+                indexOfSecond++;
+            }
+            indexOfResult++;
+        }
+
+        while (indexOfFirst < first.length) {
+            result[indexOfResult] = first[indexOfFirst];
+            indexOfResult++;
+            indexOfFirst++;
+        }
+        while (indexOfSecond < second.length) {
+            result[indexOfResult] = second[indexOfSecond];
+            indexOfResult++;
+            indexOfSecond++;
+        }
+        return result;
+    }
+}
